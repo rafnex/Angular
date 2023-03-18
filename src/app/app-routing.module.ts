16ba0446c7f99a1a6c66a6199652b915699c57ab
Wilmer Pascual <rafnex19@gmail.com>
2023-03-18 14:53:53 +0800

    ngtemplate demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgtemplateComponent } from './demo/ngtemplate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'demo/ngtemplate', component:NgtemplateComponent},
+  {path:'**', redirectTo:'/'} //we'll just redirect to main when the path does not exist; for this demo we don't need a page not found handler
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgtemplateComponent } from './demo/ngtemplate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgtemplateComponent
   ],
   imports: [
     BrowserModule,
