b3955e424b137f14a84dadd763377ca5f8dc472d
Wilmer Pascual <rafnex19@gmail.com>
2023-03-21 22:42:28 +0800

    ngTemplateOutlet demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NgcontentComponent } from './demo/ngcontent.component';
 import { NgtemplateComponent } from './demo/ngtemplate.component';
+import { NgtemplateoutletComponent } from './demo/ngtemplateoutlet.component';
 
 const routes: Routes = [
-  // {path:'demo/ngtemplate', component:NgtemplateComponent},
-  // {path:'demo/ngcontent', component:NgcontentComponent},
   {path:'demo', children:[
     {path:'ngtemplate', component:NgtemplateComponent},
-    {path:'ngcontent', component:NgcontentComponent}
+    {path:'ngcontent', component:NgcontentComponent},
+    {path:'ngtemplateoutlet', component:NgtemplateoutletComponent}
   ]},
   {path:'**', redirectTo:'/'} //we'll just redirect to main when the path does not exist; for this demo we don't need a page not found handler
 ];
